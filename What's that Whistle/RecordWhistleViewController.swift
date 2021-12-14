c25c9b20eb742b676000bea0db9ae0f7e72d42fd
NickSagan <9952728@gmail.com>
2021-12-14 15:11:23 +0300

    add RecordWhistleViewController class

---
+++ What's that Whistle/RecordWhistleViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RecordWhistleViewController.swift
+//  What's that Whistle
+//
+//  Created by Nick Sagan on 14.12.2021.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+    var stackView: UIStackView!
+}
