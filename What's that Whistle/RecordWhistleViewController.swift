ea1e78fab99044b59eb632be0e0fa309e5d9a3a7
NickSagan <9952728@gmail.com>
2021-12-14 15:25:00 +0300

    add finishRecording()

--- What's that Whistle/RecordWhistleViewController.swift
+++ What's that Whistle/RecordWhistleViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-class RecordWhistleViewController: UIViewController {
+class RecordWhistleViewController: UIViewController, AVAudioRecorderDelegate {
     var stackView: UIStackView!
     var recordButton: UIButton!
 
@@ -90,12 +90,30 @@ class RecordWhistleViewController: UIViewController {
 
     }
     
+    func finishRecording(success: Bool) {
+        view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+
+        whistleRecorder.stop()
+        whistleRecorder = nil
+
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+
+            let ac = UIAlertController(title: "Record failed", message: "There was a problem recording your whistle; please try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
     @objc func recordTapped() {
-//        if whistleRecorder == nil {
-//            startRecording()
-//        } else {
-//            finishRecording(success: true)
-//        }
+        if whistleRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
     }
     
     func startRecording() {
