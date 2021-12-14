86ee9bbb9b6777ac79671e72f81631efc1e071ee
NickSagan <9952728@gmail.com>
2021-12-14 15:00:25 +0300

    Initial Commit

---
+++ What's that Whistle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What's that Whistle
+//
+//  Created by Nick Sagan on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
