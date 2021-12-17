93aa65827dd1f0d92af48f87a701ecc184130d86
NickSagan <9952728@gmail.com>
2021-12-17 11:18:59 +0300

    Add AddCommentsViewController

---
+++ What's that Whistle/AddCommentsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddCommentsViewController.swift
+//  What's that Whistle
+//
+//  Created by Nick Sagan on 17.12.2021.
+//
+
+import UIKit
+
+class AddCommentsViewController: UIViewController {
+
+    var genre: String!
+
+    var comments: UITextView!
+    let placeholder = "If you have any additional comments that might help identify your tune, enter them here."
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
