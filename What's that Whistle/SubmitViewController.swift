45d74e5fce8bad7c63facc1d3a7f0bb0f29f9984
NickSagan <9952728@gmail.com>
2021-12-18 18:03:15 +0300

    Add SubmitViewController

---
+++ What's that Whistle/SubmitViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SubmitViewController.swift
+//  What's that Whistle
+//
+//  Created by Nick Sagan on 18.12.2021.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+    var genre: String!
+    var comments: String!
+
+    var stackView: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "You're all set!"
+        navigationItem.hidesBackButton = true
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.gray
+
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting…"
+        status.textColor = UIColor.white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+
+        spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
+    }
+}
